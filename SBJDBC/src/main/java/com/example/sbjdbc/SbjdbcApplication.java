298d06e8512dd602bdf538b346cdb408a91ec180
Bhavishya Tiwari <bhavishyatiwari2000@gmail.com>
2024-08-19 01:34:47 +0530

    Created Alian class bean which we'll link with our DB table

--- SBJDBC/src/main/java/com/example/sbjdbc/SbjdbcApplication.java
+++ SBJDBC/src/main/java/com/example/sbjdbc/SbjdbcApplication.java
@@ -1,13 +1,20 @@
 package com.example.sbjdbc;
 
+import com.example.sbjdbc.model.Alien;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SbjdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SbjdbcApplication.class, args);
+
+        ApplicationContext context = SpringApplication.run(SbjdbcApplication.class, args);
+        Alien obj = context.getBean(Alien.class);
+        obj.setId(11);
+        obj.setName("Bhavishya");
+        obj.setTech("cpp");
     }
 
 }
---
+++ SBJDBC/src/main/java/com/example/sbjdbc/repo/AlienRepo.java
@@ -0,0 +1,5 @@
+package com.example.sbjdbc.repo;
+
+public class AlienRepo {
+
+}
