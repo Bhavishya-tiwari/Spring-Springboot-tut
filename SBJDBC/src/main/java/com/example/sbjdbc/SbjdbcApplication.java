43046f675d7b8f535aa74678dc04c1b2d27e82e7
Bhavishya Tiwari <bhavishyatiwari2000@gmail.com>
2024-08-19 01:44:20 +0530

    Repo layer added

--- SBJDBC/src/main/java/com/example/sbjdbc/SbjdbcApplication.java
+++ SBJDBC/src/main/java/com/example/sbjdbc/SbjdbcApplication.java
@@ -1,6 +1,7 @@
 package com.example.sbjdbc;
 
 import com.example.sbjdbc.model.Alien;
+import com.example.sbjdbc.repo.AlienRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,10 +12,16 @@ public class SbjdbcApplication {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(SbjdbcApplication.class, args);
-        Alien obj = context.getBean(Alien.class);
-        obj.setId(11);
-        obj.setName("Bhavishya");
-        obj.setTech("cpp");
+        Alien obj1 = context.getBean(Alien.class);
+        obj1.setId(11);
+        obj1.setName("Bhavishya");
+        obj1.setTech("cpp");
+
+
+        AlienRepo repo = context.getBean(AlienRepo.class);
+        repo.save(obj1);
+        System.out.println(repo.findall());
+
     }
 
 }
--- SBJDBC/src/main/java/com/example/sbjdbc/repo/AlienRepo.java
+++ SBJDBC/src/main/java/com/example/sbjdbc/repo/AlienRepo.java
@@ -1,5 +1,19 @@
 package com.example.sbjdbc.repo;
 
-public class AlienRepo {
+import com.example.sbjdbc.model.Alien;
+import jdk.jfr.Registered;
+import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class AlienRepo {
+    public void save(Alien alien){
+        System.out.println("Data Added...");
+    }
+
+    public List<Alien> findall(){
+        return new ArrayList<Alien>();
+    }
 }
