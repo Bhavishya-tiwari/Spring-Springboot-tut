73dcb5650723c47d28d4299e39841d5baf5bf83a
Bhavishya Tiwari <bhavishyatiwari2000@gmail.com>
2024-08-18 19:58:34 +0530

    Setter injection in dependency injection

--- springproject/src/main/java/org/example/Alien.java
+++ springproject/src/main/java/org/example/Alien.java
@@ -1,11 +1,31 @@
 package org.example;
 
 public class Alien {
-    int age=19;
+    private int age;
+    private Laptop lap;
     public void code(){
+
         System.out.println("I am Coding..");
+        lap.compile();
     }
     public void printAge(){
         System.out.println(age);
+    }
+
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Age Assigned....");
+        this.age = age;
     }
 }
--- springproject/src/main/java/org/example/App.java
+++ springproject/src/main/java/org/example/App.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import Laptop.Laptop;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,10 +11,7 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
         Alien obj1 = (Alien) context.getBean("alien");
-        obj1.age=15;
-        obj1.printAge();
+        obj1.code();
 
-        Alien obj2 = (Alien) context.getBean("alien");
-        obj2.printAge();
     }
 }
