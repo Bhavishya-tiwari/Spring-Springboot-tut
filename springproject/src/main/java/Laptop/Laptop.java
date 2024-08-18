422751d66f42afa2cfd36c1ac6e1c664ef22e7f4
Bhavishya Tiwari <bhavishyatiwari2000@gmail.com>
2024-08-18 17:45:09 +0530

    first commit

---
+++ springproject/src/main/java/Laptop/Laptop.java
@@ -0,0 +1,10 @@
+package Laptop;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public void compile(){
+        System.out.println("Laptop is compiled...");
+    }
+}
