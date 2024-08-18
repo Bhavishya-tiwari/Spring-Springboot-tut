2cf6aab2ba2ca68ee29d2bcdb0e1ccb4b78b4b68
Bhavishya Tiwari <bhavishyatiwari2000@gmail.com>
2024-08-19 02:23:26 +0530

    Database initiated

--- SBJDBC/src/main/java/com/example/sbjdbc/repo/AlienRepo.java
+++ SBJDBC/src/main/java/com/example/sbjdbc/repo/AlienRepo.java
@@ -2,6 +2,8 @@ package com.example.sbjdbc.repo;
 
 import com.example.sbjdbc.model.Alien;
 import jdk.jfr.Registered;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,7 +11,21 @@ import java.util.List;
 
 @Repository
 public class AlienRepo {
+    private JdbcTemplate template;
+
+    public JdbcTemplate getTemplate() {
+        return template;
+    }
+
+    @Autowired
+    public void setTemplate(JdbcTemplate template) {
+        this.template = template;
+    }
+
     public void save(Alien alien){
+        String sql = "insert into alien (id, name, tech) values (?, ?, ?)";
+        int rowsAffected = template.update(sql, alien.getId(), alien.getName(), alien.getTech());
+        System.out.println(rowsAffected+" Rows Affected");
         System.out.println("Data Added...");
     }
 
---
+++ SBJDBC/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into alien (id, name, tech) values (99, 'maddy', 'csharp')
+insert into alien (id, name, tech) values (90, 'diago', 'cp')
+insert into alien (id, name, tech) values (89, 'aman', 'py')
